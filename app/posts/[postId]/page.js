179cc30420acbb136858f771bf46f03e656a9826
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2024-06-14 19:21:39 +0530

    first commit

---
+++ app/posts/[postId]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>dynamic page route </div>
+  )
+}
+
+export default page
---
+++ app/posts/new/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this page is for new blog</div>
+  )
+}
+
+export default page
